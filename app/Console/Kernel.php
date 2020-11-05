ab7c465d42d45fd4878056338c72473914e9b199
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2020-11-05 15:27:48 -0300

    Install laravelista/lumen-vendor-publish=^7.0.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,7 +36,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        \Laravelista\LumenVendorPublish\VendorPublishCommand::class
     ];
 
     /**
