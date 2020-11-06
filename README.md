7548fb9858a54e9d6f012e58965e6cb832972059
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2020-11-06 16:39:04 -0300

    Update database model.

--- README.md
+++ README.md
@@ -111,6 +111,51 @@ Run the migration command after docker start:
 $ php artisan migrate
 ```
 
+Or run the following DDL instructions in the file [db_schema.sql](https://github.com/julianomacielferreira/laravel-api/blob/main/db_schema.sql):
+
+```sql
+-- DROP AND RECREATE DATABASE
+DROP DATABASE IF EXISTS `laravel_api`;
+CREATE DATABASE `laravel_api`;
+USE `laravel_api`;
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `article`
+--
+CREATE TABLE IF NOT EXISTS `article` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
+  `description` TEXT NOT NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `migrations`
+--
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` VARCHAR(255) NOT NULL,
+  `batch` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+```
+
 In the root project folder start up the project:
 
 ```bash
--- db_schema.sql
+++ db_schema.sql
@@ -1,19 +1,42 @@
 -- DATABASE SCHEMA FOR LARAVEL-API
 
 -- DROP AND RECREATE DATABASE
-DROP DATABASE IF EXISTS laravel_api;
-CREATE DATABASE laravel_api;
-USE laravel_api;
+DROP DATABASE IF EXISTS `laravel_api`;
+CREATE DATABASE `laravel_api`;
+USE `laravel_api`;
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 --
 -- Table structure for table `article`
 --
 CREATE TABLE IF NOT EXISTS `article` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `title` VARCHAR(200) NOT NULL,
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(255) NOT NULL,
   `description` TEXT NOT NULL,
-  `status` TINYINT NOT NULL DEFAULT 1,
-  `created_at` DATETIME NOT NULL,
-  `updated_at` DATETIME NULL,
+  `status` TINYINT(1) NOT NULL DEFAULT '1',
+  `created_at` TIMESTAMP NULL DEFAULT NULL,
+  `updated_at` TIMESTAMP NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+--
+-- Table structure for table `migrations`
+--
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `migration` VARCHAR(255) NOT NULL,
+  `batch` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
