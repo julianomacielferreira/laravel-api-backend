4a4f61acc2d6e63367f8dae6e08f8a570cb6b8ee
Juliano Maciel <ju.maciel.ferreira@gmail.com>
2020-11-08 14:39:06 -0300

    Adjust return code.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -90,7 +90,7 @@ class AuthController extends Controller
         $token = Auth::attempt($credentials);
 
         if (!$token) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized']);
         }
 
         return $this->respondWithToken($token);
